8b8b25606ba2ef90ce9882f15075d2c919dbc8fa
Subhrajyoti <subha.ionic3@gmail.com>
2022-07-26 13:37:39 +0530

    added extra jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,14 @@ const config = {
   moduleFileExtensions: ["js", "jsx", "mjs"],
   collectCoverage: true,
   collectCoverageFrom: ["./src/**/*.js", "!**/node_modules/**"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
   coverageDirectory: "coverage",
   coverageReporters: ["text", "html", "json"],
 };
