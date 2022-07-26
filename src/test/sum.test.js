1d3387a6c919a2f27b397ac004d10a34c96d225a
Subhrajyoti <subha.ionic3@gmail.com>
2022-07-26 13:39:54 +0530

    updated test case

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,6 +1,6 @@
 const sum = require("../sum");
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(1).toBe(1);
-  // expect(sum(1, 2)).toBe(1);
+  // expect(1).toBe(1);
+  expect(sum(1, 2)).toBe(1);
 });
